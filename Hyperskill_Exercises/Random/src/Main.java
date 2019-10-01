c663b2269da044845c5e3641d597655378694ddc
Franek <panczo.mirecki@gmail.com>
2019-10-01 16:07:17 +0200

    barely started program

--- Hyperskill_Exercises/Random/src/Main.java
+++ Hyperskill_Exercises/Random/src/Main.java
@@ -3,6 +3,14 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        int a, b, n, k;
+
+        try {
+
+        } catch (Exception ex) {
+            System.err.println(ex);
+        }
 
     }
 }
